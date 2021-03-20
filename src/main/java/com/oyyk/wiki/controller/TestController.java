f0b18d3268bf50c616d4332f9ccd56736d79df25
oyyk <745691616@qq.com>
2021-03-20 21:44:02 +0800

    2-7 add a test interface for the "hello" interface

--- src/main/java/com/oyyk/wiki/controller/TestController.java
+++ src/main/java/com/oyyk/wiki/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.oyyk.wiki.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 @RestController
 public class TestController {
@@ -10,5 +12,10 @@ public class TestController {
                                  */
     public String hello(){
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World! Post. "+name;
     }
 }
