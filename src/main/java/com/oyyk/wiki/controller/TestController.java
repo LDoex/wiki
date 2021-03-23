6e953aab62917be02e5f5ac2002065a3cb65966c
oyyk <745691616@qq.com>
2021-03-23 21:09:22 +0800

    2-8 add custom configuration

--- src/main/java/com/oyyk/wiki/controller/TestController.java
+++ src/main/java/com/oyyk/wiki/controller/TestController.java
@@ -1,17 +1,22 @@
 package com.oyyk.wiki.controller;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Map;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello: Test}")
+    private String testHello;
+
     @RequestMapping("/hello") /**RequestMapping支持包括GET, POST, PUSH, DELETE在内的所有请求
                                   * 比如，要只响应GET请求，可以使用GetMapping
                                  */
     public String hello(){
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/hello/post")
