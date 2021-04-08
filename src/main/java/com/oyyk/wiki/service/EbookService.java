e4e185f7ebca0ad647183a587eede84c1684387d
oyyk <745691616@qq.com>
2021-04-08 11:12:09 +0800

    3-7 封装请求参数和返回参数

--- src/main/java/com/oyyk/wiki/service/EbookService.java
+++ src/main/java/com/oyyk/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.oyyk.wiki.service;
 
 import com.oyyk.wiki.domain.Ebook;
+import com.oyyk.wiki.domain.EbookExample;
 import com.oyyk.wiki.mapper.EbookMapper;
+import com.oyyk.wiki.req.EbookReq;
+import com.oyyk.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,8 +18,18 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list() {
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%"+req.getName()+"%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        List<EbookResp> respList = new ArrayList<>();
+        for(Ebook ebook: ebookList){
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);
+            respList.add(ebookResp);
+        }
+        return respList;
     }
 
 }
