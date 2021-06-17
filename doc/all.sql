42cd17240610a6866afd16c6bc78b2471b3dd78e
oyyk <745691616@qq.com>
2021-06-17 22:12:45 +0800

    7-2 生成分类表的Mybatis代码

--- doc/all.sql
+++ doc/all.sql
@@ -42,3 +42,38 @@ create table `demo`(
 insert into `demo` (id, name) values (1, '测试');
 
 UPDATE wikidev.ebook item SET item.category1_id = 1, item.category2_id = 2, item.doc_count = 1, item.view_count = 2, item.vote_count = 111 WHERE item.id = 1
+
+    # 分类
+drop table if exists `category`;
+create table `category` (
+                            `id` bigint not null comment 'id',
+                            `parent` bigint not null default 0 comment '父id',
+                            `name` varchar(50) not null comment '名称',
+                            `sort` int comment '顺序',
+                            primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='分类';
+
+insert into `category` (id, parent, name, sort) values (100, 000, '前端开发', 100);
+insert into `category` (id, parent, name, sort) values (101, 100, 'Vue', 101);
+insert into `category` (id, parent, name, sort) values (102, 100, 'HTML & CSS', 102);
+insert into `category` (id, parent, name, sort) values (200, 000, 'Java', 200);
+insert into `category` (id, parent, name, sort) values (201, 200, '基础应用', 201);
+insert into `category` (id, parent, name, sort) values (202, 200, '框架应用', 202);
+insert into `category` (id, parent, name, sort) values (300, 000, 'Python', 300);
+insert into `category` (id, parent, name, sort) values (301, 300, '基础应用', 301);
+insert into `category` (id, parent, name, sort) values (302, 300, '进阶方向应用', 302);
+insert into `category` (id, parent, name, sort) values (400, 000, '数据库', 400);
+insert into `category` (id, parent, name, sort) values (401, 400, 'MySQL', 401);
+insert into `category` (id, parent, name, sort) values (500, 000, '其它', 500);
+insert into `category` (id, parent, name, sort) values (501, 500, '服务器', 501);
+insert into `category` (id, parent, name, sort) values (502, 500, '开发工具', 502);
+insert into `category` (id, parent, name, sort) values (503, 500, '热门服务端语言', 503);
+
+drop table if exists `demo`;
+create table `demo` (
+                        `id` bigint not null comment 'id',
+                        `name` varchar(50) comment '名称',
+                        primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='测试';
+
+insert into `demo` (id, name) values (1, '测试');
