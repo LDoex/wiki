f9ac3110135876fa7ba37cb8c38cb3fe8b8c557a
oyyk <745691616@qq.com>
2021-06-20 11:50:44 +0800

    7-7 首页显示分类菜单

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -6,17 +6,20 @@
         v-model:openKeys="openKeys"
         :style="{ height: '100%', borderRight: 0 }"
     >
-      <a-sub-menu key="sub1">
-        <template #title>
+      <a-menu-item key="welcome">
+        <router-link to="/">
+          <MailOutlined />
+          <span>欢迎</span>
+        </router-link>
+      </a-menu-item>
+      <a-sub-menu v-for="item in level1" :key="item.id">
+        <template v-slot:title>
               <span>
                 <user-outlined />
-                subnav 111111
+                {{item.name}}
               </span>
         </template>
-        <a-menu-item key="1">option1</a-menu-item>
-        <a-menu-item key="2">option2</a-menu-item>
-        <a-menu-item key="3">option3</a-menu-item>
-        <a-menu-item key="4">option4</a-menu-item>
+        <a-menu-item v-for="child in item.children" :key="child.id">{{ child.name }}</a-menu-item>
       </a-sub-menu>
       <a-sub-menu key="sub2">
         <template #title>
@@ -73,6 +76,8 @@
 <script lang="ts">
 import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
 import axios from 'axios';
+import {Tool} from "@/util/tool";
+import {message} from "ant-design-vue";
 
 const listData: any = [];
 
@@ -93,8 +98,35 @@ export default defineComponent({
     const ebooks = ref();
     const ebooks1 = reactive({books:[]});
 
+    const level1 = ref();
+
+    /**
+     * 查询所有分类
+     */
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+
+        const data = response.data; //data = commonResp
+        if(data.success) {
+          let categorys = data.content;
+          console.log("原始数组：", categorys);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1.value);
+        } else{
+          message.error(data.message);
+        }
+      });
+    };
+
+    const handleClick = () => {
+      console.log("menu click");
+    };
+
 
     onMounted(()=>{
+      handleQueryCategory();
       axios.get("/ebook/home",{
         params:{
           page: 1,
@@ -122,6 +154,8 @@ export default defineComponent({
       { type: 'LikeOutlined', text: '156' },
       { type: 'MessageOutlined', text: '2' },
     ],
+      level1,
+      handleClick,
     }
   }
 });
