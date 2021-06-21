03250edf8d492dcb1b5d9447cdaae5a8da6cf734
oyyk <745691616@qq.com>
2021-06-21 16:43:48 +0800

    8-3 从分类管理复制出文档管理的前后端代码 遗留问题，编辑表单中，只能选择一级文档作为父文档

---
+++ src/main/java/com/oyyk/wiki/service/DocService.java
@@ -0,0 +1,124 @@
+package com.oyyk.wiki.service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.oyyk.wiki.domain.Doc;
+import com.oyyk.wiki.domain.DocExample;
+import com.oyyk.wiki.mapper.DocMapper;
+import com.oyyk.wiki.req.DocQueryReq;
+import com.oyyk.wiki.req.DocSaveReq;
+import com.oyyk.wiki.resp.DocQueryResp;
+import com.oyyk.wiki.resp.PageResp;
+import com.oyyk.wiki.util.CopyUtil;
+import com.oyyk.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DocService {
+    private static final Logger LOG = LoggerFactory.getLogger(DocService.class);
+
+    @Resource
+    private DocMapper docMapper;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    public PageResp<DocQueryResp> list(DocQueryReq req) {
+
+        DocExample docExample = new DocExample();
+        docExample.setOrderByClause("sort asc");
+        DocExample.Criteria criteria = docExample.createCriteria();
+
+        PageHelper.startPage(req.getPage(),req.getSize());
+        List<Doc> docList = docMapper.selectByExample(docExample);
+
+        PageInfo<Doc> pageInfo = new PageInfo<>(docList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+        List<DocQueryResp> respList = new ArrayList<>();
+//        for(Doc doc: docList){
+////            DocResp docResp = new DocResp();
+////            BeanUtils.copyProperties(doc, docResp);
+        //对象复制
+//            DocResp docResp = CopyUtil.copy(doc, DocResp.class);
+//            respList.add(docResp);
+//        }
+
+        //列表复制
+        List<DocQueryResp> list = CopyUtil.copyList(docList, DocQueryResp.class);
+
+        PageResp<DocQueryResp> pageResp = new PageResp();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
+    }
+
+    public List<DocQueryResp> all() {
+
+        DocExample docExample = new DocExample();
+        docExample.setOrderByClause("sort asc");
+        List<Doc> docList = docMapper.selectByExample(docExample);
+
+        //列表复制
+        List<DocQueryResp> list = CopyUtil.copyList(docList, DocQueryResp.class);
+
+        return list;
+    }
+
+    public PageResp<DocQueryResp> home(DocQueryReq req) {
+
+        DocExample docExample = new DocExample();
+        DocExample.Criteria criteria = docExample.createCriteria();
+        //动态sql，name不为空才进行模糊匹配
+
+        PageHelper.startPage(req.getPage(),req.getSize());
+        List<Doc> docList = docMapper.selectByExample(docExample);
+
+        PageInfo<Doc> pageInfo = new PageInfo<>(docList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+        List<DocQueryResp> respList = new ArrayList<>();
+//        for(Doc doc: docList){
+////            DocResp docResp = new DocResp();
+////            BeanUtils.copyProperties(doc, docResp);
+        //对象复制
+//            DocResp docResp = CopyUtil.copy(doc, DocResp.class);
+//            respList.add(docResp);
+//        }
+
+        //列表复制
+        List<DocQueryResp> list = CopyUtil.copyList(docList, DocQueryResp.class);
+
+        PageResp<DocQueryResp> pageResp = new PageResp();
+        pageResp.setList(list);
+        return pageResp;
+    }
+
+    /**
+     * 保存
+     * @param req
+     */
+    public void save(DocSaveReq req){
+        Doc doc = CopyUtil.copy(req, Doc.class);
+        if(ObjectUtils.isEmpty(req.getId())){
+            //新增
+            doc.setId(snowFlake.nextId());
+            docMapper.insert(doc);
+        } else {
+            //更新
+            docMapper.updateByPrimaryKey(doc);
+        }
+    }
+
+    public void delete(Long id){
+        docMapper.deleteByPrimaryKey(id);
+    }
+
+}
