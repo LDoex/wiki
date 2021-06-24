66567738abaf9d4075f31cd0e1a6c068d361c345
oyyk <745691616@qq.com>
2021-06-24 11:17:01 +0800

    8-12 为文档内容增加wangeditor样式，和富文本框保持一致，所见即所得

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -7,14 +7,14 @@
         <a-col :span="6">
           <a-tree
               v-if="level1.length > 0"
-              @select="onsSelect"
+              @select="onSelect"
               :tree-data="level1"
               :replaceFields="{title: 'name', key: 'id', value: 'id'}"
               :defaultExpandAll="true"
           />
         </a-col>
         <a-col :span="18">
-
+          <div class="wangeditor" :innerHTML="html"></div>
         </a-col>
       </a-row>
   </a-layout-content>
@@ -53,6 +53,7 @@ export default defineComponent({
      **/
     const level1 = ref();
     level1.value = [];
+    const html = ref();
 
 
     /**
@@ -76,6 +77,28 @@ export default defineComponent({
       });
     };
 
+    /**
+     * 内容查询 handleQuery相当于一个对象实例
+     **/
+    const handleQueryContent = (id: number) => {
+      axios.get("/doc/find-content/"+id).then((response) => {
+        const data = response.data;
+
+        if(data.success){
+          html.value = data.content;
+        } else{
+          message.error(data.message);
+        }
+      });
+    };
+
+    const onSelect = (selectedKeys: any, info: any) => {
+      console.log('selected', selectedKeys, info);
+      if(Tool.isNotEmpty(selectedKeys)){
+        handleQueryContent(selectedKeys[0]);
+      }
+    };
+
 
 
     onMounted(() => {
@@ -85,7 +108,58 @@ export default defineComponent({
     return {
       // docs,
       level1,
+      html,
+      onSelect,
     }
   }
 });
 </script>
+
+<style>
+/* wangeditor默认样式, 参照: http://www.wangeditor.com/doc/pages/02-%E5%86%85%E5%AE%B9%E5%A4%84%E7%90%86/03-%E8%8E%B7%E5%8F%96html.html */
+/* table 样式 */
+.wangeditor table {
+border-top: 1px solid #ccc;
+border-left: 1px solid #ccc;
+}
+.wangeditor table td,
+.wangeditor table th {
+border-bottom: 1px solid #ccc;
+border-right: 1px solid #ccc;
+padding: 3px 5px;
+}
+.wangeditor table th {
+border-bottom: 2px solid #ccc;
+text-align: center;
+}
+
+/* blockquote 样式 */
+.wangeditor blockquote {
+display: block;
+border-left: 8px solid #d0e5f2;
+padding: 5px 10px;
+margin: 10px 0;
+line-height: 1.4;
+font-size: 100%;
+background-color: #f1f1f1;
+}
+
+/* code 样式 */
+.wangeditor code {
+display: inline-block;
+*display: inline;
+*zoom: 1;
+background-color: #f1f1f1;
+border-radius: 3px;
+padding: 3px 5px;
+margin: 0 3px;
+}
+.wangeditor pre code {
+display: block;
+}
+
+/* ul ol 样式 */
+.wangeditor ul, ol {
+margin: 10px 0 10px 20px;
+}
+</style>
