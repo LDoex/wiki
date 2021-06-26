b3b68860f46eb8b6b6845dc1f20566742603aa11
oyyk <745691616@qq.com>
2021-06-26 10:18:35 +0800

    9-9 修复BUG，原因：computed加上花括号需要加return，不加花括号return可以省略

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -11,12 +11,10 @@ import store from "@/store";
 export default defineComponent({
   name: 'the-footer',
   setup(){
-    const user = computed(() => {store.state.user});
+    const user = computed(() => {return store.state.user;});
     return{
       user,
     }
   }
-
-
 });
 </script>
