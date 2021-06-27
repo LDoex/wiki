2aa29c5a4c97b89ea71d8687327d5bdc4ff5aec1
oyyk <745691616@qq.com>
2021-06-27 10:19:17 +0800

    10-5 定时任务示例(介绍了目前流行的定时任务框架quartz)

--- src/main/java/com/oyyk/wiki/config/WikiApplication.java
+++ src/main/java/com/oyyk/wiki/config/WikiApplication.java
@@ -7,11 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 //@ComponentScan({"com.oyyk","com.test"}) /**扫描多个包的写法*/
 @ComponentScan("com.oyyk")
 @SpringBootApplication
 @MapperScan("com.oyyk.wiki.mapper")
+@EnableScheduling
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 
