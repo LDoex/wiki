17b2c6d9628b36721e6b5e503ebd807ce0634ecf
oyyk <745691616@qq.com>
2021-06-27 11:00:08 +0800

    10-6 增加定时器，定时执行电子书信息更新SQL，增加日志打印耗时

--- src/main/java/com/oyyk/wiki/job/DocJob.java
+++ src/main/java/com/oyyk/wiki/job/DocJob.java
@@ -1,6 +1,8 @@
 package com.oyyk.wiki.job;
 
 import com.oyyk.wiki.service.DocService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -12,13 +14,17 @@ public class DocJob {
     @Resource
     private DocService docService;
 
+    private static final Logger LOG = LoggerFactory.getLogger(DocJob.class);
+
 
    /**
     * 每30秒更新电子书信息
     */
    @Scheduled(cron = "5/30 * * * * ?")
    public void cron(){
+       long start = System.currentTimeMillis();
+       LOG.info("开始更新电子书下的文档数据");
        docService.updateEbookInfo();
+       LOG.info("更新电子书下的文档数据结束，耗时：{}毫秒", System.currentTimeMillis()-start);
    }
-
 }
