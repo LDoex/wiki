a99a6cae4745bd5003e4d811990652cf91dcc833
oyyk <745691616@qq.com>
2021-06-28 21:21:06 +0800

    11-6 后端增加获取统计数值接口

---
+++ src/main/java/com/oyyk/wiki/controller/EbookSnapshotController.java
@@ -0,0 +1,28 @@
+package com.oyyk.wiki.controller;
+
+
+import com.oyyk.wiki.resp.CommonResp;
+import com.oyyk.wiki.resp.StatisticResp;
+import com.oyyk.wiki.service.EbookSnapshotService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook-snapshot")
+public class EbookSnapshotController {
+
+    @Resource
+    private EbookSnapshotService ebookSnapshotService;
+
+    @GetMapping("/get-statistic")
+    public CommonResp getStatistic() {
+        List<StatisticResp> statisticResp = ebookSnapshotService.getStatistic();
+        CommonResp<List<StatisticResp>> commonResp = new CommonResp<>();
+        commonResp.setContent(statisticResp);
+        return commonResp;
+    }
+}
--- src/main/java/com/oyyk/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/oyyk/wiki/mapper/EbookSnapshotMapperCust.java
@@ -1,5 +1,10 @@
 package com.oyyk.wiki.mapper;
 
+import com.oyyk.wiki.resp.StatisticResp;
+
+import java.util.List;
+
 public interface EbookSnapshotMapperCust {
     public void genSnapshot();
+    public List<StatisticResp> getStatistic();
 }
