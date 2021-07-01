51958668f6148f44c5f6f6df9e692db52f622180
oyyk <745691616@qq.com>
2021-07-01 10:54:56 +0800

    添加自适应布局

--- web/src/main.ts
+++ web/src/main.ts
@@ -7,6 +7,7 @@ import 'ant-design-vue/dist/antd.css';
 import * as Icons from '@ant-design/icons-vue';
 import axios from 'axios'
 import {Tool} from "@/util/tool";
+import 'lib-flexible';
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER
 
